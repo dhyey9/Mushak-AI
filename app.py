b0965f2ff7c40f00b369fc4b69df9d7ae0f12095
Dhyey <dhyeynick@gmail.com>
2024-09-07 18:05:25 -0700

    Update app.py

--- app.py
+++ app.py
@@ -18,15 +18,14 @@ generation_config = {
 
 # Define the model
 model = genai.GenerativeModel(
-    model_name="gemini-1.0-pro",
+    model_name="gemini-1.5-flash",
     generation_config=generation_config
 )
 
 # Function to generate a response
 def get_gemini_response(prompt):
-    persona_prompt = f"Mushak, Lord Ganesha's mouse, is answering: {prompt}"
-    response = model.generate_content([persona_prompt])
-    return response[0].text.strip()  # Return the text of the first response
+    response = model.generate_content([prompt])
+    return response.text.strip()  # Directly access the text attribute
 
 # Streamlit app interface
 st.set_page_config(page_title="Gemini LLM App", page_icon=":robot:", layout="wide")
@@ -40,54 +39,3 @@ if st.button("Generate"):
         st.write("Mushak's Response:", response)
     else:
         st.write("Please enter a prompt.")
-# import streamlit as st
-# import os
-# from dotenv import load_dotenv
-# import google.generativeai as genai
-
-# # GOOGLE_API_KEY='AIzaSyDJsCtThBvPKy8rLbK4AeKOMEACARKccMk'
-
-# load_dotenv()
-# genai.configure(api_key='AIzaSyDJsCtThBvPKy8rLbK4AeKOMEACARKccMk')
-# generation_config = {
-#   "temperature": 0.9,
-#   "top_p": 1,
-#   "max_output_tokens": 2048,
-#   "response_mime_type": "text/plain",
-# }
-
-# model = genai.GenerativeModel(
-#   model_name="gemini-1.0-pro",
-#   generation_config=generation_config
-#   # safety_settings = Adjust safety settings
-#   # See https://ai.google.dev/gemini-api/docs/safety-settings
-# )
-
-# response = model.generate_content([
-#   "input: What would be an ideal name of the mouse?",
-#   "output: Mushak",
-#   "input: Tell me about yourself.",
-#   "output: Hi I am Mushak AI, I can help you with anything you want to learn about Ganesha.",
-#   "input: Who is Lord Ganesh?",
-#   "output: Lord Ganesh is the Son of Lord Shiv and Parvati",
-#   "input: How does lord Ganesh Look?",
-#   "output: He Looks like an elephant headed, smiling and removes obstacles from a devotees path.",
-#   "input: Who is lord ganesh",
-#   "output: ",
-# ])
-
-
-# def get_gemini_response(prompt):
-#     response = model.generate_content(prompt)
-#     return response.text
-
-# # st.title("Gemini LLM App")
-# st.set_page_config(page_title="Gemini LLM App", page_icon=":robot:", layout="wide")
-# user_input = st.text_input("Enter your prompt:")
-
-# if st.button("Generate"):
-#     if user_input:
-#         response = get_gemini_response(user_input)
-#         st.write("Response:", response)
-#     else:
-#         st.write("Please enter a prompt.")
